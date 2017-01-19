e0ebaca696a593cd14a7e07dadd9a27f915c3fb6
dubis <dubois.michel@gmail.com>
2017-01-19 21:23:54 +0100

    Update Android.mk
    I corrected the typo
    LOCAL_CFLAGS += -Wstrict-prototypes -Wmissing-prototypes -Wshadow -Wpointer-arith -Wcast-qual -Winline -MMD -fPIC
    to
    LOCAL_CFLAGS += -Wstrict-prototypes -Wmissing-prototypes -Wshadow -Wpointer-arith -Wcast-qual -Winline -MMD -fPIE
    
    And  I added 
    LOCAL_LDFLAGS += -fPIE -pie
    For error only position independent executables (PIE) are supported message

--- Android.mk
+++ Android.mk
@@ -24,7 +24,8 @@ LOCAL_PATH:= $(call my-dir)
 ################## build iwlib ###################
 include $(CLEAR_VARS)
 LOCAL_SRC_FILES := iwlib.c
-LOCAL_CFLAGS += -Wstrict-prototypes -Wmissing-prototypes -Wshadow -Wpointer-arith -Wcast-qual -Winline -MMD -fPIC
+LOCAL_CFLAGS += -Wstrict-prototypes -Wmissing-prototypes -Wshadow -Wpointer-arith -Wcast-qual -Winline -MMD -fPIE
+LOCAL_LDFLAGS += -fPIE -pie
 LOCAL_MODULE:= libiw
 LOCAL_STATIC_LIBRARIES := libcutils libc libm
 include $(BUILD_STATIC_LIBRARY)
@@ -32,7 +33,8 @@ include $(BUILD_STATIC_LIBRARY)
 ################## build iwconfig ###################
 include $(CLEAR_VARS)
 LOCAL_SRC_FILES := iwconfig.c
-LOCAL_CFLAGS += -Wstrict-prototypes -Wmissing-prototypes -Wshadow -Wpointer-arith -Wcast-qual -Winline -MMD -fPIC
+LOCAL_CFLAGS += -Wstrict-prototypes -Wmissing-prototypes -Wshadow -Wpointer-arith -Wcast-qual -Winline -MMD -fPIE
+LOCAL_LDFLAGS += -fPIE -pie
 LOCAL_MODULE:= iwconfig
 LOCAL_STATIC_LIBRARIES := libcutils libc libm libiw
 LOCAL_MODULE_PATH := $(TARGET_OUT_OPTIONAL_EXECUTABLES) # install to system/xbin
@@ -41,7 +43,8 @@ include $(BUILD_EXECUTABLE)
 ################## build iwlist ###################
 include $(CLEAR_VARS)
 LOCAL_SRC_FILES := iwlist.c iwlib.h
-LOCAL_CFLAGS += -Wstrict-prototypes -Wmissing-prototypes -Wshadow -Wpointer-arith -Wcast-qual -Winline -MMD -fPIC
+LOCAL_CFLAGS += -Wstrict-prototypes -Wmissing-prototypes -Wshadow -Wpointer-arith -Wcast-qual -Winline -MMD -fPIE
+LOCAL_LDFLAGS += -fPIE -pie
 LOCAL_MODULE:= iwlist
 LOCAL_STATIC_LIBRARIES := libcutils libc libm libiw
 LOCAL_MODULE_PATH := $(TARGET_OUT_OPTIONAL_EXECUTABLES) # install to system/xbin
